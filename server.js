545a54831ee57d52531890e288b1fc90b342ee0c
U-GESBO\Shengbo <gesboge@gmail.com>
2015-11-19 02:11:43 -0500

    deleteaccount + login time out

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('static_files'));
 var sqlite3 = require('sqlite3').verbose();
 var formidable = require('formidable');
+var path = require('path');
 var util = require('util');
 var fs = require('fs-extra');
-
+var mkdirp = require('mkdirp'); 
 var db = new sqlite3.Database('WanU.db');
 
 db.run('CREATE TABLE IF NOT EXISTS WanU_user (email TEXT PRIMARY KEY, name TEXT, log_in INTEGER, language TEXT, city TEXT, pictures INTEGER)');
@@ -25,14 +26,10 @@ db.run('CREATE TABLE IF NOT EXISTS WanU_user (email TEXT PRIMARY KEY, name TEXT,
 app.post('/users', function (req, res) {
   var postBody = req.body;
   db.all("SELECT * FROM WanU_user WHERE email='"+postBody.email+"'", function (err, row) {
-
     if (row.length==0)
     {
-      db.run("INSERT INTO WanU_user (email, name, log_in, pictures) VALUES (?,?,?,?)", [postBody.email,postBody.name,0,0]);
-   
-      var mkdirp = require('mkdirp');   
+      db.run("INSERT INTO WanU_user (email, name, log_in, pictures) VALUES (?,?,?,?)", [postBody.email,postBody.name,0,0]);  
       mkdirp('static_files/'+postBody.email, function (err) {});
-      
       res.send('Account created.');
       return;
     }
@@ -86,18 +83,15 @@ app.get('/user_pref/*', function (req, res) {
 });
 
 // get user preference
-app.get('/user_image/*', function (req, res) {
+app.get('/image/*', function (req, res) {
   var userEmail = req.params[0]; 
-  var fs    = require("fs");
-  console.log(userEmail);
-  var files=fs.readdirSync('C:/Users/ywang/Desktop/WanU/static_files/'+userEmail);
-  console.log(files)
-  console.log("Number of photos: "+files.length);
-  console.log(files);
+//  console.log(userEmail);
+  var files=fs.readdirSync(__dirname+'/static_files/'+userEmail);
+//  console.log(files);
+//  console.log("Number of photos: "+files.length);
   var user_photos={photos: files};
   res.send(user_photos);
-  return;
-      });
+});
 
 
 // update user preferance
@@ -110,13 +104,14 @@ app.post('/image/*', function (req, res) {
   var userEmail = req.params[0]; 
   var form = new formidable.IncomingForm();
   form.uploadDir = __dirname + "\\static_files\\" + userEmail;
+  form.keepExtensions = true;
   form.parse(req, function(err, fields, files) {
     db.all("SELECT * FROM WanU_user WHERE email=?", [userEmail], function(err, row){
       var numPictures = row[0].pictures;
       numPictures++;
       db.run("UPDATE WanU_user SET pictures=? where email=?",[numPictures,userEmail]);
       fs.renameSync(files.image.path,
-                    __dirname + "\\static_files\\" + userEmail+"\\"+numPictures+".jpg");
+                    __dirname + "\\static_files\\" + userEmail+"\\"+numPictures+path.extname(files.image.path));
       res.send("Image Successfully Uploaded.");
     });
   });
@@ -140,56 +135,17 @@ app.get('/users', function (req, res) {
 
   res.send(allUsernames);
 });
-
-
-// UPDATE a user's profile with the data given in POST
-//
-// To test with curl, run:
-//   curl -X PUT --data "job=bear_wrangler&pet=bear.jpg" http://localhost:3000/users/Philip
-app.put('/users/*', function (req, res) {
-  var nameToLookup = req.params[0]; // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
-  for (var i = 0; i < fakeDatabase.length; i++) {
-    var e = fakeDatabase[i];
-    if (e.name == nameToLookup) {
-      // update all key/value pairs in e with data from the post body
-      var postBody = req.body;
-      for (key in postBody) {
-        var value = postBody[key];
-        e[key] = value;
-      }
-
-      res.send('OK');
-      return; // return early!
-    }
-  }
-
-  res.send('ERROR'); // nobody in the database matches nameToLookup
-});
-
-
-// DELETE a user
-//
-// To test with curl, run:
-//   curl -X DELETE http://localhost:3000/users/Philip
-//   curl -X DELETE http://localhost:3000/users/Jane
-app.delete('/users/*', function (req, res) {
-  var nameToLookup = req.params[0]; // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
-  for (var i = 0; i < fakeDatabase.length; i++) {
-    var e = fakeDatabase[i];
-    if (e.name == nameToLookup) {
-      fakeDatabase.splice(i, 1); // remove current element at index i
-      res.send('OK');
-      return; // return early!
-    }
-  }
-
-  res.send('ERROR'); // nobody in the database matches nameToLookup
-});
-
-
 */
+
+app.delete('/user/*', function (req, res) {
+  var userEmail = req.params[0]; 
+  db.run("DELETE FROM WanU_user WHERE email=?",[userEmail]);
+  fs.removeSync(__dirname+"/static_files/"+userEmail);
+  res.send("OK");
+});
+
+
+
 // start the server on http://localhost:3000/
 var server = app.listen(3000, function () {
   var port = server.address().port;
